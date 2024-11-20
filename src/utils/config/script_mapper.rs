fa93a3ee3fc83bb2b4b3bf811feaf81ed58c56da
YuQiJaang <rdjksp@gmail.com>
2024-11-20 20:17:57 +0800

    change format str

--- src/utils/config/script_mapper.rs
+++ src/utils/config/script_mapper.rs
@@ -173,7 +173,7 @@ impl ScriptMapper {
         let err_str = String::from_utf8(stderr)?;
         let status = status.to_string();
         Ok(format!(
-            "Script Exit With Status:\n {} \n Output len {}:\n {} \n Error len {}:\n {} \n",
+            "Script Exit With Status:\n {} \n Output(len {}):\n {} \n Error(len {}):\n {} \n",
             status,
             &out_str,
             out_str.len(),
